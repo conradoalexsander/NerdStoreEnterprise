7cf74847921b06e78cf62e68b65d2c47fde798b4
Conrado Alexsander Soares Gonçalves <conradoalexsander@hotmail.com>
2021-08-03 16:51:46 -0300

    Added basic login method

---
+++ src/services/NSE.Identity.API/Models/UserViewModels.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Identity.API.Models
+{
+    public class RegisterUser
+    {
+        [Required(ErrorMessage="The field {0} is obligatory")]
+        [EmailAddress(ErrorMessage="The field {0} is in wrong format")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "The field {0} is obligatory")]
+        [StringLength(100, ErrorMessage = "The field {0} must has between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage ="Unmatching passwords")]
+        public string PasswordConfirmation { get; set; }
+    }    
+    
+    public class LoginUser
+    {
+        [Required(ErrorMessage="The field {0} is obligatory")]
+        [EmailAddress(ErrorMessage="The field {0} is in wrong format")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "The field {0} is obligatory")]
+        [StringLength(100, ErrorMessage = "The field {0} must has between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+
+    }
+}
