f5a11adfd86c92d481ab4106504c82e1086eac39
conrado.goncalves <53683786+conradoalexsander@users.noreply.github.com>
2021-11-24 08:37:01 +0100

    Added Login and Register views

---
+++ src/web/NSE.WebApp.MVC/Controllers/IdentityController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        [Route("new-account")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("new-account")]
+        public async Task<IActionResult> Register(RegisterUser registerUser)
+        {
+            if (!ModelState.IsValid) return View(registerUser);
+
+            if (false) return View(registerUser);
+
+            return RedirectToAction("Index", "Home");
+
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(LoginUser loginUser)
+        {
+            if (!ModelState.IsValid) return View(loginUser);
+
+            if (false) return View(loginUser);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("logout")]
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Models/UserViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.WebApp.MVC.Models
+{
+    public class RegisterUser
+    {
+        [Required(ErrorMessage = "The field {0} can not be empty.")]
+        [EmailAddress(ErrorMessage = "Invalid format for field {0}")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} can not be empty.")]
+        [StringLength(100, ErrorMessage = "The field {0} must have between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "The field {0} can not be empty.")]
+        [Compare("Password", ErrorMessage = "Password and confirmation mismatching")]
+        public string PasswordConfirmation { get; set; }
+    }
+
+    public class LoginUser
+    {
+        [Required(ErrorMessage = "The field {0} can not be empty.")]
+        [EmailAddress(ErrorMessage = "Invalid format for field {0}")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} can not be empty.")]
+        [StringLength(100, ErrorMessage = "The field {0} must have between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
