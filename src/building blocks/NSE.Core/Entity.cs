a8102bead26de07e382f2a9a82c2314ac96da92f
OS-conradogoncalves <102364014+OS-conradogoncalves@users.noreply.github.com>
2022-04-11 00:23:14 +0200

    Fixed Entity.cs operator override

--- src/building blocks/NSE.Core/Entity.cs
+++ src/building blocks/NSE.Core/Entity.cs
@@ -26,20 +26,20 @@ namespace NSE.Core
             return $"{GetType().Name} [Id={Id}]";
         }
 
-        public static bool operator !=(Entity a, Entity b)
-        {
-            return !(a == b);
-        }
-
         public static bool operator ==(Entity a, Entity b)
         {
-            if (ReferenceEquals((a, null) && ReferenceEquals(b, null)))
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
                 return true;
             
-            if (ReferenceEquals((a, null) || ReferenceEquals(b, null)))
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
                 return false;
 
             return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
         }
     }
 }
